91214d30d74fd179a773afb2f0a47035f57c9ea4
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2022-04-26 10:27:12 +0300

    Update Utils.js

--- src/Utils.js
+++ src/Utils.js
@@ -1,6 +1,7 @@
 export const reportWebVitals = onPerfEntry => {
     
     if (onPerfEntry && onPerfEntry instanceof Function) {
+        
         import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
             getCLS(onPerfEntry);
             getFID(onPerfEntry);
