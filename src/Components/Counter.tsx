0db299799cbaf3b2bdc6bb578b1c2f833fd0fa2b
PGorokhov <pgorokhov@yandex.ru>
2022-04-26 20:00:25 +0300

    add css module

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import counter from "../Store/counter";
 import {observer} from "mobx-react-lite";
+import classes from './styles.module.css'
 
 interface IProps {}
 
 export const Counter: React.FC<IProps> = observer(() => {
     return (
-      <>
+      <div className={classes.counterContainer}>
           <div>
               {counter.total}
           </div>
           <button onClick={()=>counter.increment()} >inc</button>
           <button onClick={()=>counter.decrement()} >dec</button>
-      </>
+      </div>
     )
 })
