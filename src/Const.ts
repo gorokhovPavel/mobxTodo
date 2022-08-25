4c3509112eaa739eae037d1a1e0e2aa2e16469ec
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2022-08-25 18:54:37 +0300

    Update Const.ts

--- src/Const.ts
+++ src/Const.ts
@@ -4,6 +4,5 @@ import {ITodo} from "./Models";
 export const defaultTodoList: ITodo[]  = [
     {id: 0, title:  'go to the Shop', isCompleted: true },
     {id: 1, title:  'Set like'     , isCompleted: false },
-    
     {id: 2, title: 'Watch youTube' , isCompleted: false },
 ]
